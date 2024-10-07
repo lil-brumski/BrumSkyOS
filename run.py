6cbde9486277bdafe32e0b1cc2d2ea989f40a922
David Tamaratare Oghenebrume <davidbrume7@gmail.com>
2024-10-07 15:50:47 +0100

    Small changes
    Modified 4 files.

--- run.py
+++ run.py
@@ -2,7 +2,10 @@ import subprocess
 import os
 
 if os.path.exists('build'):
-   subprocess.run(["./BrumSkyOS.o"], cwd = 'build')
+   if os.path.exists('build/BrumSkyOS.o'):
+      subprocess.run(["./BrumSkyOS.o"], cwd = 'build')
+   else:
+      print("Bro...there isn't an executable in the build directory, try running the setup.py file first.")
 else:
    print("Error: 'build' does not exist!\nRun the setup.py first before running this one.")
    
--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ else:
    subprocess.run(["cmake", ".."], cwd = "build")
    subprocess.run(["make"], cwd = "build")
    if os.path.exists("build/BrumSkyOS.o"):
-      print("Enter 'python run.py' file to run the executable located in the 'build' directory.")
+      print("Enter 'python run.py' to run the executable located in the 'build' directory.")
    else:
       print("Error: BrumSkyOS.o wasn't built!")
